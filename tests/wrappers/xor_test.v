e27e0a9d545e7d65382279fbca79dce9bff4553e
emmanueljohnson797 <65718585+emmanueljohnson797@users.noreply.github.com>
2022-10-16 17:51:35 +0530

    x
    x

---
+++ tests/wrappers/xor_test.v
@@ -0,0 +1,16 @@
+module xor_test(
+	input wire a,
+	input wire b,
+	output wire y
+);
+xor_gate xor_gate(
+	.a(a),
+	.b(b),
+	.y(y)
+);
+
+initial begin
+	$dumpfile("or.vcd");
+	$dumpvars;
+end
+endmodule
--- tests/xor_test.py
+++ tests/xor_test.py
@@ -4,4 +4,12 @@ from cocotb.triggers import Timer
 
 @cocotb.test()
 async def dut_test(dut):
-    assert 1, "Test Not Implemented Error"
+    a = (0, 0, 1, 1)
+    b = (0, 1, 0, 1)
+    y = (0, 1, 1, 0)
+
+    for i in range(4):
+        dut.a.value = a[i]
+        dut.b.value = b[i]
+        await Timer(1, 'ns')
+        assert dut.y.value == y[i], f"Error at iteration {i}"
